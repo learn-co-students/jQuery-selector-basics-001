25467193b25715885210350219c3cb2ab56bf8f1
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-06 10:48:43 -0500

    creates solution

--- js/selectors.js
+++ js/selectors.js
@@ -7,3 +7,22 @@ $(document).ready(function(){
 });
 
 // declare your functions here...
+function h1Selector(){
+  return $('h1');
+}
+
+function liInOlSelector(){
+  return $('ol li');
+}
+
+function linkSelector(){
+  return $('#box4.box5 a');
+}
+
+function imageSelector(){
+  return $('img[alt="cat sleeping"]');
+}
+
+function checkboxInputSelector(){
+  return $('input[type="checkbox"').wrap('<span class="pinky"></span>');
+}
