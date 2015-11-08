1ca4a4b2cc6ce7f332fe12b4f0cad6768efb63cc
github username <github email address>
2015-11-08 13:32:40 -0500

    query query query

--- js/selectors.js
+++ js/selectors.js
@@ -8,3 +8,23 @@ $(document).ready(function(){
 });
 
 // declare your functions here...
+
+var h1Selector = function(){
+  return $("h1")
+}
+
+var liInOlSelector = function(){
+  return $("ol li")
+}
+
+var linkSelector = function(){
+  return $("#box4.box5 a")
+}
+
+var imageSelector = function(){
+  return $('img[alt="cat sleeping"]')
+}
+
+var checkboxInputSelector = function(){
+  return $(":checkbox").wrap("<span class='pinky' />")
+}
