a261107f94ef93ad580b1278cf7dce804eaae3b4
Garett Arrowood <garettarrowood@gmail.com>
2015-11-25 19:55:24 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -7,4 +7,22 @@ $(document).ready(function(){
   checkboxInputSelector();
 });
 
-// declare your functions here...
+function h1Selector() {
+  return $('h1');
+}
+
+function liInOlSelector() {
+  return $('ol li');
+}
+
+function linkSelector() {
+  return $('#box4 a');
+}
+
+function imageSelector() {
+  return $('img[alt="cat sleeping"]');
+}
+
+function checkboxInputSelector() {
+  return $('input[type="checkbox"').wrap( "<span class='pinky'></div>" );;
+}
